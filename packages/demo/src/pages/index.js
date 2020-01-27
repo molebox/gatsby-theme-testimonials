f87cc4aa79adbe880b6c0b1eebae13696850afd0
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2020-01-27 13:38:38 +0100

    changed demo styling

--- packages/demo/src/pages/index.js
+++ packages/demo/src/pages/index.js
@@ -3,6 +3,7 @@ import { jsx } from "theme-ui";
 import styled from "@emotion/styled";
 import {TestimonialSection} from 'gatsby-theme-testimonials';
 import DefaultLayout from "../components/DefaultLayout";
+import { FaNpm, FaGithub } from 'react-icons/fa';
 
 const Header = styled.header`
     display: flex;
@@ -24,9 +25,17 @@ const Header = styled.header`
     @media (min-width: 48em) {
         display: flex;
         justify-content: center;
-      & > h1,
-      h2, p {
+    
+        & > h1 {
+            font-size: 2em;
+            color: rgb(0, 31, 63, 0.8);
+            text-shadow: 9px 9px 16px rgb(163, 177, 198, 0.6), -9px -9px 16px rgba(255, 255, 255, 0.6);
+        }
+
+      & > h2, p {
         font-size: 1em;
+        color: rgb(0, 31, 63, 1);
+        text-shadow: 9px 9px 16px rgb(163, 177, 198, 0.6), -9px -9px 16px rgba(255, 255, 255, 0.6);
       }
     }
 `;
@@ -36,6 +45,30 @@ const Container = styled.main`
     border-radius: 0.5em;
     padding: 1em;
     box-shadow: inset 6px 6px 13px rgb(163, 177, 198, 0.4), inset -6px -6px 13px rgba(255, 255, 255, 0.7);
+`;
+
+const Box = styled.div`
+    margin: 1em 2em;
+    border-radius: 0.5em;
+    padding: 1em;
+    box-shadow: 9px 9px 16px rgb(163, 177, 198, 0.6), -9px -9px 16px rgba(255, 255, 255, 0.6);
+
+    &:hover {
+        box-shadow: inset 9px 9px 16px rgb(163, 177, 198, 0.4), inset -9px -9px 16px rgba(255, 255, 255, 1);
+    }
+`;
+
+const Links = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+
+    // Desktop
+    @media (min-width: 48em) {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+    }
 `;
 
 export default () => {
@@ -71,24 +104,45 @@ export default () => {
             }}>
                 The default styling is <a href="https://uxdesign.cc/neumorphism-in-user-interfaces-b47cef3bf3a6">neumorphism</a>, this can be overriden by shadowing the TestimonialCard component.
             </p>
+            <Links>
+            <Box>
             <p sx={{
                 fontFamily: 'Nunito',
                 fontWeight: '400',
-                margin: '1em 2em'
+                margin: '1em 2em',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
             }}>
-                NPM package: <a href="https://www.npmjs.com/package/gatsby-theme-testimonials">gatsby-theme-testimonials</a>
+                <FaNpm sx={{
+                    color: '#001f3f',
+                    fontSize: '2em',
+                    marginRight: '1em'
+                    }}/> <a href="https://www.npmjs.com/package/gatsby-theme-testimonials">gatsby-theme-testimonials</a>
             </p>
+            </Box>
+            <Box>
             <p sx={{
                 fontFamily: 'Nunito',
                 fontWeight: '400',
-                margin: '1em 2em'
+                margin: '1em 2em',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
             }}>
-                GitHub repository: <a href="https://github.com/molebox/gatsby-theme-testimonials">github.com/molebox/gatsby-theme-testimonials</a>
+                <FaGithub sx={{
+                    color: '#001f3f',
+                    fontSize: '2em',
+                    marginRight: '1em'
+                    }}/> <a href="https://github.com/molebox/gatsby-theme-testimonials">github.com/molebox/gatsby-theme-testimonials</a>
             </p>
+            </Box>
+            </Links>
+           
         </Header>
         <Container>
         <TestimonialSection/>
-        </Container>
+        </Container>   
     </DefaultLayout>
     )
 }
